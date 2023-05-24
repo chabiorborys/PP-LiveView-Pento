c167c2bad91c1358f39fdf6ab7ee4338e08d7e6f
Borys Chabior <bchabior@totalbet.pl>
2023-05-24 16:20:17 +0200

    Added new live route. Added new WrongLive module with mount and render functions.

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ import Config
 # Configure your database
 config :pento, Pento.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "coderslab",
   hostname: "localhost",
   database: "pento_dev",
   stacktrace: true,
