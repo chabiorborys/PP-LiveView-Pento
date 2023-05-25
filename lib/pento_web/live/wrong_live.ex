80d63b8ff3e23b5541a582a1ceacd47825ec36e1
Borys Chabior <bchabior@totalbet.pl>
2023-05-25 12:58:59 +0200

    Patern matching on handle_cast/3, allowing player to guess a correct number, or not.

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -2,7 +2,44 @@ defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+    {
+      :ok,
+      assign(
+        socket,
+        score: 0,
+        message: "Make a guess:",
+        target: :rand.uniform(10) |> to_string()
+        )
+    }
+  end
+
+  def handle_event("guess", %{"number" => guess}, %{assigns: %{target: guess}} = socket) do
+    correct_message = "You guessed! The number was #{guess}"
+    score = socket.assigns.score + 1
+
+    {
+      :noreply,
+      assign(
+        socket,
+        message: correct_message,
+        score: score
+      )
+    }
+  end
+
+  def handle_event("guess", %{"number" => guess}, %{assigns: %{target: _target}} = socket) do
+    wrong_message = "Your guess: #{guess}. Wrong. Guess again."
+
+    score = socket.assigns.score - 1
+
+    {
+      :noreply,
+      assign(
+        socket,
+        message: wrong_message,
+        score: score
+        )
+    }
   end
 
   def render(assigns) do
